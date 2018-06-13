6e065341464f65c52f27fe5fb0bcb1dfbb38f1c7
SamMahani <sam.amirmahani@gmail.com>
2018-06-13 15:14:15 -0700

    PSP (problem solving pattern): specification -> Visualization

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -16,3 +16,36 @@ var Stack = function() {
 
   return someInstance;
 };
+
+
+/*
+Specification::IOCE
+  Input: 
+    any single value of any type
+  Output: 
+    .push will not return a value
+    .pop will return popped value
+    .size will return a numerical value
+    side effects: values within Stack function will be modified
+  Constraints:
+    none
+  Edge Cases:
+    popping an empty stack should do nothing
+
+
+Justification::what are you creating
+  Stack is a collection. It's purpose is to hold values in a last in first out order - similar to a stack of plates.
+
+Explanation::
+  At input, a value will be stored in memory somewhere. The only outputs come from pop and size methods. When pop is called, it will only 
+  return a value if something exists within the stack. Size will return either 0 or the size of the stack based on prior input.
+
+Visualization::
+  Done
+  
+Approximation::
+
+Verification::
+
+Implementation::
+*/

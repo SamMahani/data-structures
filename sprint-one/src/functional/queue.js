edf63805d314f467fbf26f76babe21097f12d229
SamMahani <sam.amirmahani@gmail.com>
2018-06-13 13:18:19 -0700

    refactored pseudocode, added code to methods

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,34 +1,30 @@
 var Queue = function() {
 // Use an object with numeric keys to store values
   var storage = {};
-
-
-
   var someInstance = {};
+  // Implement the methods below
 
 
-// Implement the methods below
+  var currFirstIndex = 0;
+  var currLastIndex = 0;
 
-  //enqueue: insert value into back of collection
   someInstance.enqueue = function(value) {
-    //function to set last node of collection
-      //
-      //
-
-    //function to keep track of previous last node's key number
+    //store value by adding one to currentEnd (automatically updates currentEnd)
+    storage[currLastIndex] = value;
+    currLastIndex += 1;
 
     //call size.increment
-
+    this.size.increment();
   };
 
-  //dequed: first item in collection is deleted
   someInstance.dequeue = function() {
-    //function to delete first node of collection
-      //
-      //
-    //function to keep track of previous first node's key number
-    
-    //call size.decrement
+  //function to delete first node of collection
+  var deleteKey = function() {
+    delete storage[currFirstIndex];
+    currFirstIndex += 1;
+
+
+    deleteKey();
   };
 
   someInstance.size = function() {
@@ -37,6 +33,8 @@ var Queue = function() {
     //function to decrement size
 
     //function to get current value
+
+    //return size
   };
 
   return someInstance;

4fb0020922e9ca13ef70576619621e4320626282
SamMahani <sam.amirmahani@gmail.com>
2018-06-14 11:00:13 -0700

    all but two tests from shared behavior passing

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -22,18 +22,28 @@ var queueMethods = {};
 //add enqueue method with value input
 queueMethods.enqueue = function(value) {
   //add input value to storage at last index property
-  
+  this.storage[this.currLastIndex] = value;
   //increment last index property
+  this.currLastIndex++;
   //increment current size
+  this.currSize++;
 };
 
 //add dequeue method
 queueMethods.dequeue = function() {
   //if last index propety is greater than or equeal to first index property
+  if(this.currLastIndex >= this.currFirstIndex) {
     //temporarily save the value at first index in storage
+    var tempValToReturn = this.storage[this.currFirstIndex];
     //delete the value at first index in storage
-    //decrement current size
+    delete this.storage[this.currFirstIndex];
+    //increment first index property
+    this.currFirstIndex++;
+    //decrement current size property
+    this.currSize++;
     //return temporarily saved value
+    return tempValToReturn;
+  }
 };
 
 //add size method

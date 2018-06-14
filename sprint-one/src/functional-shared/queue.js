a80ea48a90efecb992eaf118ec6374d823ca482a
SamMahani <sam.amirmahani@gmail.com>
2018-06-14 10:45:04 -0700

    completed pseudocode

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,35 +1,33 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  //create instance object
+  //add collector property object
+  //add current size property 
+  //add current first index property 
+  //add current last index property
+  //add methods via extend
+  //return instance
 };
 
 var queueMethods = {};
+//add enqueue method with value input
+  //increment last index property
+  //add input value to object at last index property
+  //increment current size
+
+//add dequeue method
+  //if last index propety is greater than or equeal to first index property
+    //temporarily save the value at first index in object
+    //delete the value at first index in object
+    //decrement current size
+    //return temporarily saved value
+
+//add size method
+  //return current size
 
 
 
 
 
-/*
-Specification::IOCE
-  Input: 
-
-  Output: 
-    side effects:
-
-  Constraints:
-   
-  Edge Cases:
-
-
-Justification::what are you creating
-
-Explanation::
-
-Visualization::
-
-Approximation::
- 
-Verification::
-
-Implementation::
-*/

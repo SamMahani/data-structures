a89053e89d484bbca53c412c31c34ce06d8103ad
SamMahani <sam.amirmahani@gmail.com>
2018-06-17 22:43:59 -0700

    passing all tests

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,46 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    //add storage property object
+    this.storage = {};
+    //add current size property 
+    this.currSize = 0;
+    //add current first index property 
+    this.currFirstIndex = 0;
+    //add current last index property
+    this.currLastIndex = 0;
   }
-
+  
+  //add enqueue method with value input
+  enqueue(value) {
+    //add input value to storage at last index property
+    this.storage[this.currLastIndex] = value;
+    //increment last index property
+    this.currLastIndex++;
+    //increment current size
+    this.currSize++;
+  }
+  
+  //add dequeue method
+  dequeue() {
+    //if last index propety is greater than or equeal to first index property
+    if (this.currSize > 0) {
+      //temporarily save the value at first index in storage
+      var tempValToReturn = this.storage[this.currFirstIndex];
+      //delete the value at first index in storage
+      delete this.storage[this.currFirstIndex];
+      //increment first index property
+      this.currFirstIndex++;
+      //decrement current size property
+      this.currSize--;
+      //return temporarily saved value
+      return tempValToReturn;
+    }
+  }
+  
+  //add size method
+  size() {
+    //return current size
+    return this.currSize;
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,40 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    //initialize currIndex to -1
+    this.currIndex = -1;
+    //initialize empty storage
+    this.storage = {};
   }
-
+  
+  //initialize push
+  push(value) {
+    //increment currIndex
+    this.currIndex++;
+    //create key value pair using this currIndex and this value in storage
+    this.storage[this.currIndex] = value;
+  }
+  
+  //initialize pop 
+  pop() {
+    //if currIndex is greater than or equal to 0
+    if (this.currIndex >= 0) {
+  
+      var tempIndex = this.currIndex;
+      //save the value of currIndex at storage
+      var tempVar = this.storage[this.currIndex];
+      //delete the value of currIndex at storage
+      delete this.storage[this.currIndex];
+      //decrement currIndex
+      this.currIndex--;
+      //return the saved value
+      return tempVar;
+    }
+  };
+  
+  //initialize size
+  size() {
+    //return this currIndex + 1
+    return this.currIndex + 1;
+  }
 }
